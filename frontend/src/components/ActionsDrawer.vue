95043939ab5e4d2aa5a893e81c51f12d3bbec18e
Henrique Gabriel Moraes Denoni <henriquedevprofessional@gmail.com>
2024-03-11 21:46:08 -0400

    First commit

---
+++ frontend/src/components/ActionsDrawer.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-item
+    clickable
+  >
+    <q-item-section
+      avatar
+    >
+      <q-icon name="window" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label class="text-bold text-subtitle1">
+        Grid.
+      </q-item-label>
+      <q-item-label caption>
+        Grid do game
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+
+  <q-item
+    clickable
+  >
+    <q-item-section
+      avatar
+    >
+      <q-icon name="table_view" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label class="text-bold text-subtitle1">
+        Configuração do Grid.
+      </q-item-label>
+      <q-item-label caption>
+        Altere o background e outras configurações refente ao grid.
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+
+  <q-item
+    clickable
+  >
+    <q-item-section
+      avatar
+    >
+      <q-icon name="person" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label class="text-bold text-subtitle1">
+        Personagens.
+      </q-item-label>
+      <q-item-label caption>
+        Adicionar e incluir personagens no grid.
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+
+  <q-item
+    clickable
+  >
+    <q-item-section
+      avatar
+    >
+      <q-icon name="person" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label class="text-bold text-subtitle1">
+        Configuração geral.
+      </q-item-label>
+      <q-item-label caption>
+        Tema de cores e outras configurações referentes aos sistema.
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'ActionsDrawerComponent',
+})
+</script>
---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header 
+      bordered
+      class="bg-primary"
+    >
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+        <q-toolbar-title>
+          TableFlow RPG
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <ActionsDrawer />
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import ActionsDrawer from 'src/components/ActionsDrawer.vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup() {
+    const data = reactive({
+      leftDrawerOpen: ref(false)
+    })
+
+    return {
+      ...toRefs(data)
+    }
+  },
+  components: {
+    ActionsDrawer
+  },
+});
+</script>
